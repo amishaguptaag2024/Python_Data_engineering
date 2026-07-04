cf435090cf41072e5f08cadbb8a97aab70ec7359
“amisha” <amishaguptaag2024@gmail.com>
2026-07-04 11:37:24 +0530

    sql1

--- SQL/Leetcode_SQL/problem_set1.sql
+++ SQL/Leetcode_SQL/problem_set1.sql
@@ -3,3 +3,22 @@
 --pr1:https://leetcode.com/problems/tournament-winners/
 --pr2:https://leetcode.com/problems/longest-winning-streak/
 --pr3:https://leetcode.com/problems/top-three-wineries/description/
+
+-- Create table If Not Exists Person (Id int, Email varchar(255));
+-- Truncate table Person;
+-- insert into Person (id, email) values ('1', 'john@example.com');
+-- insert into Person (id, email) values ('2', 'bob@example.com');
+-- insert into Person (id, email) values ('3', 'john@example.com');
+
+delete from Person where id in (
+    with cte as (
+        select *,
+            dense_rank() over(partition by email order by id) as rnk
+        from person 
+    )
+    select id
+    from cte
+    where rnk != 1
+);
+
+select * from Person;
